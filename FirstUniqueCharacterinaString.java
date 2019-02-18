14dea4cfe600cf97c7f584a1c25ef4ea00a41d6e
Dhananjay131 <djwid92@gmail.com>
2019-02-18 08:54:13 -0800

    Update FirstUniqueCharacterinaString.java

--- FirstUniqueCharacterinaString.java
+++ FirstUniqueCharacterinaString.java
@@ -5,7 +5,7 @@ class Solution {
         for(char c = 'a'; c <= 'z'; c++){
             int index = s.indexOf(c);
             if(index != -1 && index == s.lastIndexOf(c)){
-                result = Math.min(result, index);
+                result = Math.min(result, index); //index being the least, starting from left of string
             }
         }
         return result == s.length() ? -1 : result;
