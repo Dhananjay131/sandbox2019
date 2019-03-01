b7624300b7953d0b3326fc8acf7d755db42b7703
Dhananjay131 <djwid92@gmail.com>
2019-02-28 22:03:26 -0800

    Update LongestPalindromicSubstring.java

--- LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring.java
@@ -1,28 +1,31 @@
-public class Solution {
-private int lo, maxLen;
-
-public String longestPalindrome(String s) {
-	int len = s.length();
-	if (len < 2)
-		return s;
-	
-    for (int i = 0; i < len-1; i++) {
-     	extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
-     	extendPalindrome(s, i, i+1); //assume even length.
+class Solution {
+    private int start, maxLen=0;
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        if(n<2)
+            return s;
+        
+        for(int i=0; i<n-1; i++){
+            stretchPal(s,i,i);
+            stretchPal(s,i,i+1);
+        }
+        return s.substring(start, start+maxLen);
     }
-    return s.substring(lo, lo + maxLen);
+    
+    public void stretchPal(String s, int lo, int hi){
+        while(lo>=0 && hi<s.length() && s.charAt(lo)==s.charAt(hi)){
+            //lo and hi check if put afterwards, gives arrayindex exception
+            lo--;
+            hi++;
+        }
+        
+        if(hi-lo-1 >= maxLen){   //= is optional, will give latest
+            start = lo+1;
+            maxLen = hi-lo-1;
+        }
+    }
+    
 }
-
-private void extendPalindrome(String s, int j, int k) {
-	while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
-		j--;
-		k++;
-	}
-	if (maxLen < k - j - 1) {
-		lo = j + 1;
-		maxLen = k - j - 1;
-	}
-}}
 
 //DP but very poor runtime
 class Solution {
